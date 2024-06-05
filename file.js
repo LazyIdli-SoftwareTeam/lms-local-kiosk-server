a4542510b0a0c9347456b27e063540b47f218a73
notadevps <sahilarora8527397612@gmail.com>
2024-06-05 12:26:27 +0530

    added configs for all the type of videos

--- file.js
+++ file.js
@@ -7,24 +7,33 @@ const createFile = async (d, config, fileInfo, key) => {
   if (exists) {
     await fs.unlinkSync(path + '/' + fileInfo.fileInfo.name);
   }
-  console.log(exists);
   if (folder) {
-    fs.writeFileSync(path + '/' + fileInfo.fileInfo.name, fileInfo.chunks, {
-      flag: 'wx',
-    });
+    await fs.writeFileSync(
+      path + '/' + fileInfo.fileInfo.name,
+      fileInfo.chunks,
+      {
+        flag: 'wx',
+      }
+    );
   }
 };
 module.exports.kioskConfig = async (d, config) => {
   for (const el of Object.keys(d[config])) {
-    if (el === 'mediaForTopAd') {
+    if (
+      el === 'mediaForTopAd' ||
+      el === 'mediaForBottomOffers' ||
+      el === 'instructionVideo'
+    ) {
       const publishedFiles = [];
-      for (const fileInfo of d[config][el]) {
-        if (fileInfo.archive && fileInfo.archive) return;
+      let tempO = d[config][el];
+      for (const fileInfo of tempO) {
+        if (fileInfo.archive) continue;
         await createFile(d, config, fileInfo, el);
         publishedFiles.push(fileInfo.fileInfo.name);
       }
-      console.log(publishedFiles);
       const dirPath = __dirname + '/files/' + d[config].customId + '/' + el;
+      const existPath = await fs.existsSync(dirPath);
+      if (!existPath) continue;
       fs.readdir(dirPath, async (err, files) => {
         if (err) console.log(err);
         for (const file of files) {
@@ -36,4 +45,26 @@ module.exports.kioskConfig = async (d, config) => {
       });
     }
   }
+
+  const nwKiosk = { ...d };
+  for (const el of Object.keys(nwKiosk.kiosk)) {
+    if (
+      el === 'mediaForTopAd' ||
+      el === 'mediaForBottomOffers' ||
+      el === 'instructionVideo'
+    ) {
+      if (!el || !nwKiosk.kiosk[el]) continue;
+      const newTemp = nwKiosk.kiosk[el].map((e) => {
+        return {
+          fileName: e.fileInfo.name,
+          id: e.fileInfo.id,
+          archive: e.fileInfo.archive || false,
+          all: e.fileInfo.all || false,
+        };
+      });
+      nwKiosk.kiosk[el] = newTemp;
+    }
+  }
+  console.log(nwKiosk);
+  await fs.writeFileSync('config.json', JSON.stringify(nwKiosk));
 };
