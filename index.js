1c07cdebc5c6b74c04ffca92bd66b93de05c80b3
notadevps <sahilarora8527397612@gmail.com>
2024-06-05 14:32:39 +0530

    made express server static files

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const io = require('socket.io-client');
 const fs = require('fs');
+const express = require('express');
+const app = express();
+const config = require('./config.json');
 const { checkFolder } = require('./folder');
+const cors = require('cors');
 const { createFile, kioskConfig } = require('./file');
 const KIOSKID = 'PTK-001';
 const socket = io('http://localhost:3000');
@@ -15,8 +19,29 @@ socket.on('getFiles', async (d) => {
     console.log(e);
   }
 });
+app.use(express.json());
+app.use(cors());
+app.use(express.static('files'));
+app.get('/links', (req, res) => {
+  try {
+    const nwKiosk = { ...config };
+    nwKiosk.kiosk.mediaForTopAd = nwKiosk.kiosk.mediaForTopAd.filter(
+      (e) => !e.archive
+    );
+    nwKiosk.kiosk.mediaForBottomOffers =
+      nwKiosk.kiosk.mediaForBottomOffers.filter((e) => !e.archive);
 
+    return res.status(200).json({ message: 'Done!', data: nwKiosk });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({ message: 'Some error occurred' });
+  }
+});
 
 socket.on('error', (e) => {
   console.log(e);
+});
+
+app.listen(3300, () => {
+  console.log('app is working on port 3300');
 });
